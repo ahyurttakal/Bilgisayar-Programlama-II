835e499a9f6e459689fce521cbe7e8f66ab432f3
Ahmet Haşim Yurttakal <38229970+ahy032@users.noreply.github.com>
2022-04-20 11:17:00 +0300

    Create arraylist.cs

---
+++ arraylist.cs
@@ -0,0 +1,9 @@
+            ArrayList dizilistesi = new ArrayList();
+            dizilistesi.Add(03);
+            dizilistesi.Add("Afyon");
+            dizilistesi.Add('+');
+            dizilistesi.Add(true);
+            foreach (var item in dizilistesi)
+            {
+                listBox1.Items.Add(item);
+            }
